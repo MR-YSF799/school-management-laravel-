4b18c567da19dfd82ef36a89a06aee936990f51c
youssef lahmami <yousseflahmami70@gmail.com>
2026-03-15 18:03:50 +0000

    modified the migration file of course

--- database/migrations/2026_03_15_180117_create_courses_table.php
+++ database/migrations/2026_03_15_180117_create_courses_table.php
@@ -11,10 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('courses', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+       Schema::create('courses', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->text('description')->nullable();
+        $table->integer('credits');
+        $table->timestamps();
+    });
     }
 
     /**

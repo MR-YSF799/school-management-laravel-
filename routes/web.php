147396dddf7764064d422523e06765833cc3b18e
youssef lahmami <yousseflahmami70@gmail.com>
2026-03-15 18:36:56 +0000

    fix the error by import the CourseController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 use App\Http\Controllers\StudentController;
-
+use App\Http\Controllers\CourseController;   // ← cette ligne doit exister
 Route::resource('students', StudentController::class);
 Route::resource('courses',  CourseController::class);
